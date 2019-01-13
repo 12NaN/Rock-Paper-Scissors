4d2a399c1613fc873e136754fa1d4464da678fc7
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-01-13 18:55:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar/>
+        <Home/>
+        <Footer/>
       </div>
     );
   }
