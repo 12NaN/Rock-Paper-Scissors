ebbbc3d1372b5ac37750e488a12dd41e5d0c313b
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-01-13 16:20:52 -0500

    Updates

---
+++ precache-manifest.000bbc9c97f87e499a68a892464b610f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f2f0cdf0c221f8d98a",
+    "url": "/rock-paper-scissors/static/js/runtime~main.62f2f0cd.js"
+  },
+  {
+    "revision": "88be18d95d0ff0ad3ac8",
+    "url": "/rock-paper-scissors/static/js/main.88be18d9.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/rock-paper-scissors/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "88be18d95d0ff0ad3ac8",
+    "url": "/rock-paper-scissors/static/css/main.5825907e.chunk.css"
+  },
+  {
+    "revision": "932dc818ab92035247781dd722de00bf",
+    "url": "/rock-paper-scissors/index.html"
+  }
+];
