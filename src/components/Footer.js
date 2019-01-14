31b447fe8ac459ea135b124bc1558684b0121454
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-01-13 19:18:18 -0500

    Revert "Revert "update""
    This reverts commit 43988479dd6b8908eb9f1436f3f348b5c3db1561.

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './style.css';
+export default class Footer extends Component {
+  render(){
+    return(
+      <footer>
+        <span className="navbar-text">
+          <h1> By Frank Orefice (12NaN) </h1>
+        </span>
+      </footer>
+    );
+  }
+}
